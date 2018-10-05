ae1aaa4b2419c3852f18b856bcb9ad6a1f1a399d
unknown <chenghaowang03@gmail.com>
2018-10-05 13:49:44 -0400

    Debug the codes

--- Lab2/HexLight.do
+++ Lab2/HexLight.do
@@ -1,45 +1,59 @@
 vlib work
 
-vlog -timescale 1ns/1ns part3Segment7.v
+vlog -timescale 1ns/1ns HexLight.v
 
-vsim segmentDecorder
+vsim HexLight
 
 log {/*}
 
 add wave {/*}
 
-force {SW[3]} 1
-force {SW[2]} 0
-force {SW[1]} 1
-force {SW[0]} 0
+force {Input[3]} 1
+force {Input[2]} 0
+force {Input[1]} 1
+force {Input[0]} 0
 run 10ns
 
-force {SW[3]} 1
-force {SW[2]} 0
-force {SW[1]} 1
-force {SW[0]} 1
+force {Input[3]} 1
+force {Input[2]} 0
+force {Input[1]} 1
+force {Input[0]} 1
 run 10ns
 
-force {SW[3]} 1
-force {SW[2]} 1
-force {SW[1]} 0
-force {SW[0]} 0
+force {Input[3]} 1
+force {Input[2]} 1
+force {Input[1]} 0
+force {Input[0]} 0
 run 10ns
 
-force {SW[3]} 0
-force {SW[2]} 0
-force {SW[1]} 0
-force {SW[0]} 1
+force {Input[3]} 0
+force {Input[2]} 0
+force {Input[1]} 0
+force {Input[0]} 1
 run 10ns
 
-force {SW[3]} 0
-force {SW[2]} 0
-force {SW[1]} 1
-force {SW[0]} 0
+force {Input[3]} 0
+force {Input[2]} 0
+force {Input[1]} 1
+force {Input[0]} 0
 run 10ns
 
-force {SW[3]} 0
-force {SW[2]} 0
-force {SW[1]} 1
-force {SW[0]} 1
+force {Input[3]} 0
+force {Input[2]} 0
+force {Input[1]} 1
+force {Input[0]} 1
+run 10ns
+
+#15 -> F
+force {Input[3]} 1
+force {Input[2]} 1
+force {Input[1]} 1
+force {Input[0]} 1
+run 10ns
+
+#14 -> E
+force {Input[3]} 1
+force {Input[2]} 1
+force {Input[1]} 1
+force {Input[0]} 0
 run 10ns
--- Lab2/HexLight.v
+++ Lab2/HexLight.v
@@ -1,44 +1,42 @@
-//SW[3:0] data input, 4 switch
-//HEX0[6:0] output for 7 segment light
+//Input[3:0] data input, usually is 4 SW
+//Output[6:0] output for a Hex light
 
-module segmentDecorder(HEX0,SW);
-	input [9:0] SW;
-	output [6:0] HEX0;
+module HexLight(Output,Input);
+	input [3:0] Input;
+	output [6:0] Output;
 
-	assign HEX0[0] = ((!SW[3])&(!SW[2])&(!SW[1])&(SW[0]))|	//0001
-		            ((!SW[3])&(SW[2])&(!SW[1])&(!SW[0]))|	//0100
-		            ((SW[3])&(SW[2])&(!SW[1])&(SW[0]))|	//1101
-		            ((SW[3])&(!SW[2])&(SW[1])&(SW[0]));	//1011
+	assign Output[0] = ((!Input[3])&(!Input[2])&(!Input[1])&(Input[0]))|	//0001
+		            ((!Input[3])&(Input[2])&(!Input[1])&(!Input[0]))|	//0100
+		            ((Input[3])&(Input[2])&(!Input[1])&(Input[0]))|	//1101
+		            ((Input[3])&(!Input[2])&(Input[1])&(Input[0]));	//1011
 
-	assign HEX0[1] = ((SW[3])&(SW[1])&(SW[0]))|	                //1x11
-		  ((SW[2])&(SW[1])&(!SW[0]))|			//x110
-		  ((SW[3])&(SW[2])&(!SW[1])&(!SW[0]))|		//1100
-		  ((!SW[3])&(SW[2])&(!SW[1])&(SW[0]));		//0101
+	assign Output[1] = ((Input[3])&(Input[1])&(Input[0]))|	                //1x11
+		            ((Input[2])&(Input[1])&(!Input[0]))|		//x110
+		            ((Input[3])&(Input[2])&(!Input[1])&(!Input[0]))|	//1100
+		            ((!Input[3])&(Input[2])&(!Input[1])&(Input[0]));	//0101
 		 
-	assign HEX0[2] = ((SW[3])&(SW[2])&(!SW[1])&(!SW[0]))|	//1100
-		  ((SW[3])&(SW[2])&(SW[1]))|			//111x
-		  ((!SW[3])&(!SW[2])&(SW[1])&(!SW[0]));	//0010
+	assign Output[2] = ((Input[3])&(Input[2])&(!Input[1])&(!Input[0]))|	//1100
+		  ((Input[3])&(Input[2])&(Input[1]))|			//111x
+		  ((!Input[3])&(!Input[2])&(Input[1])&(!Input[0]));		//0010
 
-	assign HEX0[3] = ((!SW[3])&(SW[2])&(!SW[1])&(!SW[0]))|	//0100
-		  ((!SW[3])&(!SW[2])&(!SW[1])&(SW[0]))|	//0001
-		  ((!SW[3])&(SW[2])&(SW[1])&(SW[0]))|		//0111
-		   ((SW[3])&(!SW[2])&(SW[1])&(!SW[0]))|	//1010
-		  ((SW[3])&(SW[2])&(SW[1])&(SW[0]));		//1111
+	assign Output[3] = ((!Input[3])&(Input[2])&(!Input[1])&(!Input[0]))|	//0100
+		  ((!Input[3])&(!Input[2])&(!Input[1])&(Input[0]))|		//0001
+		  ((!Input[3])&(Input[2])&(Input[1])&(Input[0]))|		//0111
+		   ((Input[3])&(!Input[2])&(Input[1])&(!Input[0]))|		//1010
+		  ((Input[3])&(Input[2])&(Input[1])&(Input[0]));		//1111
 		
-	assign HEX0[4] = ((!SW[3])&(SW[0]))|			//0xx1
-		  ((!SW[3])&(SW[2])&(!SW[1]))|		//010x
-		  ((!SW[2])&(!SW[1])&(SW[0]));		//x001
+	assign Output[4] = ((!Input[3])&(Input[0]))|			//0xx1
+		  ((!Input[3])&(Input[2])&(!Input[1]))|			//010x
+		  ((!Input[2])&(!Input[1])&(Input[0]));			//x001
 
-	assign HEX0[5] = ((!SW[3])&(!SW[2])&(SW[0]))|		//00x1
-		  ((!SW[3])&(!SW[2])&(SW[1]))|		//001x
-		  ((!SW[3])&(SW[1])&(SW[0]))|			//0x11
-		  ((SW[3])&(SW[2])&(!SW[1])&(SW[0]));		//1101
+	assign Output[5] = ((!Input[3])&(!Input[2])&(Input[0]))|		//00x1
+		  ((!Input[3])&(!Input[2])&(Input[1]))|			//001x
+		  ((!Input[3])&(Input[1])&(Input[0]))|			//0x11
+		  ((Input[3])&(Input[2])&(!Input[1])&(Input[0]));		//1101
 
-	assign HEX0[6] = ((!SW[3])&(!SW[2])&(!SW[1]))|		//000x
-		  ((SW[3])&(SW[2])&(!SW[1])&(!SW[0]))|		//1100
-		  ((!SW[3])&(SW[2])&(SW[1])&(SW[0]));		//0111
-
-
+	assign Output[6] = ((!Input[3])&(!Input[2])&(!Input[1]))|		//000x
+		  ((Input[3])&(Input[2])&(!Input[1])&(!Input[0]))|		//1100
+		  ((!Input[3])&(Input[2])&(Input[1])&(Input[0]));		//0111
 endmodule
 
 
